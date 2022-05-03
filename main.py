05536bc6c81b0b5693983ac62a61cb6bc6c9e579
pvittor2 <104523202+pvittor2@users.noreply.github.com>
2022-05-03 20:38:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame
 import os
 import time
 import random
+
 pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter Tutorial")
+pygame.display.set_caption("Space Shooter")
 
 # Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
